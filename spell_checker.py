0f2528c109e5e5925c37317cbdc4b4c617b01096
Sandesh Thapa <63294026+Sandesh-Thapa@users.noreply.github.com>
2023-01-09 18:46:16 +0545

    handle exception for checking spelling having code contents

--- spell_checker.py
+++ spell_checker.py
@@ -21,24 +21,31 @@ def checkSpelling(text):
 
 def main(website):
     results = {'h1': [], 'h2': []}
-    try:
-        page = requests.get(website, headers={'User-Agent': ''}, timeout=60)
-        if page.status_code == 404:
-            results["error"] = "404 Site not found!"
-        elif page.status_code == 403:
-            results["error"] = "403 Forbidden!"
-        else:
-            h1tags, h2tags = scrapePage(page)
-            for h1 in h1tags:
+
+    page = requests.get(website, headers={'User-Agent': ''}, timeout=60)
+    print(page.status_code)
+    if page.status_code == 404:
+        results["error"] = "404 Site not found!"
+    elif page.status_code == 403:
+        results["error"] = "403 Forbidden!"
+    elif page.status_code == 200:
+        h1tags, h2tags = scrapePage(page)
+        for h1 in h1tags:
+            try:
                 h1 = checkSpelling(h1.text)
                 if len(h1['response']['errors']) > 0:
                     results['h1'].append(h1['response']['errors'])
-                
-            for h2 in h2tags:
+            except:
+                continue
+            
+        for h2 in h2tags:
+            try:
                 h2 = checkSpelling(h2.text)
                 if len(h2['response']['errors']) > 0:
-                    results['h2'].append(h1['response']['errors'])
-    except:
+                    results['h2'].append(h2['response']['errors'])
+            except:
+                continue
+    else:
         results["error"] = "Something went wrong! Try Again!"
 
     return results
